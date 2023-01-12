7d3325f9ff0d4e45a64738d9649301353a33d70a
canubemy2meow <70944427+61010822@users.noreply.github.com>
2023-01-12 15:17:02 +0700

    Update pushcode.groovy

--- vars/pushcode.groovy
+++ vars/pushcode.groovy
@@ -1,3 +1,10 @@
 def call(){
-    sh './jenkins/push/push.sh'
+    IMAGE="maven-proj"
+    echo "** Logging in **"
+    docker login -u pittimonr -p $PASS
+    echo "** Tagging image **"
+    docker tag $IMAGE:$BUILD_TAG pittimonr/$IMAGE:$BUILD_TAG
+    echo "** Pushing image **"
+    docker push pittimonr/$IMAGE:$BUILD_TAG 
+    //sh './jenkins/push/push.sh'
 }
